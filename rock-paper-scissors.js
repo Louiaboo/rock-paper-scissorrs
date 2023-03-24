390fa99916854bd2b9804b0138d5d0a86eaf78e0
loui <106045306+Louiaboo@users.noreply.github.com>
2023-03-24 15:34:47 +0800

    Add game mechanic function

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -8,3 +8,21 @@ function getComputerChoice() {
         return "Scissors"
     }
 }
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+        return "It's a tie!"
+    } else if (playerSelection === "r" && computerSelection === "p") {
+        return "Paper Beats Rock, You Lose!"
+    } else if (playerSelection === "r" && computerSelection === "s") {
+        return "Rock Beats Scissors, You Win!"
+    } else if (playerSelection === "p" && computerSelection === "s") {
+        return "Scissors Beats Paper, You Lose!"
+    } else if (playerSelection === "p" && computerSelection === "r") {
+        return "Paper Beats Rock, You Win!"
+    } else if (playerSelection === "s" && computerSelection === "p") {
+        return "Scissors Beats Paper, You Win!"
+    } else if (playerSelection === "s" && computerSelection === "r") {
+        return "Rock Beats Scissors, You Lose!"
+    }
+}
