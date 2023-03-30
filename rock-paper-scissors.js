a5df2b37a10da2f1ed27d7866e0e68c1ca17a4c8
loui <106045306+Louiaboo@users.noreply.github.com>
2023-03-30 11:49:16 +0800

    Add basic functions to buttons

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -11,18 +11,25 @@ function getComputerChoice() {
 
 function playRound(playerSelection, computerSelection) {
     if (playerSelection === computerSelection) {
+        console.log("It's a tie!");
         return "It's a tie!"
     } else if (playerSelection === "r" && computerSelection === "p") {
+        console.log("computerwin!");
         return "computer win!"
     } else if (playerSelection === "r" && computerSelection === "s") {
+        console.log("player win!");
         return "player win!"
     } else if (playerSelection === "p" && computerSelection === "s") {
+        console.log("computer win!");
         return "computer win!"
     } else if (playerSelection === "p" && computerSelection === "r") {
+        console.log("player win!");
         return "player win!"
     } else if (playerSelection === "s" && computerSelection === "p") {
+        console.log("player win!");
         return "player win!"
     } else if (playerSelection === "s" && computerSelection === "r") {
+        console.log("player win!");
         return "computer win!"
     }
 }
@@ -31,19 +38,26 @@ function game() {
     let playerScore = 0;
     let computerScore = 0;
 
-    while (playerScore < 5 && computerScore < 5) {
-        let playerSelection = prompt("Rock Paper or Scissors").charAt(0).toLowerCase();
-        if (playRound(playerSelection, getComputerChoice()) === "computer win!") {
-            computerScore += 1;
-        } else if (playRound(playerSelection, getComputerChoice()) === "computer win!") {
-            playerScore += 1;
-        }
-        console.log(`Computer Score is ${computerScore}`)
-        console.log(`Your score is ${playerScore}`)
-    }
+    // while (playerScore < 5 && computerScore < 5) {
+    //     let playerSelection = prompt("Rock Paper or Scissors").charAt(0).toLowerCase();
+    //     if (playRound(playerSelection, getComputerChoice()) === "computer win!") {
+    //         computerScore += 1;
+    //     } else if (playRound(playerSelection, getComputerChoice()) === "computer win!") {
+    //         playerScore += 1;
+    //     }
+    //     console.log(`Computer Score is ${computerScore}`)
+    //     console.log(`Your score is ${playerScore}`)
+    // }
     if (computerScore === 5) {
         return "You Lose!"
     } else if (playerScore === 5) {
         return "You Win!"
     }
 }
+
+const buttons = document.querySelectorAll("button");
+buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+        playRound((button.className).charAt(0).toLowerCase(), getComputerChoice());
+    });
+});
