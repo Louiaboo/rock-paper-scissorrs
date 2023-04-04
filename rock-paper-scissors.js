ad1b8be5e75d6f2ee3f24eeac757dd0c2bd1fe66
loui <106045306+Louiaboo@users.noreply.github.com>
2023-04-04 14:24:04 +0800

    Add reset/play again functionality

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -4,6 +4,8 @@ const title = document.querySelector(".title");
 const buttons = document.querySelectorAll("button");
 const pScore = document.querySelector("#player");
 const cScore = document.querySelector("#computer");
+const buttonContainer = document.querySelector(".buttons");
+const reset = document.createElement("button");
 
 function getComputerChoice() {
     let choice = Math.floor(Math.random() * 3);
@@ -26,11 +28,20 @@ function giveScore(winner) {
 
 function isGameOver() {
     if (playerScore > 4 || computerScore > 4) {
-        playerScore = 0;
-        computerScore = 0;
         return true;
     }
     return false;
+}
+
+function resetGame() {
+    while (buttonContainer.firstChild) {
+        buttonContainer.removeChild(buttonContainer.lastChild);
+    }
+    buttonContainer.append(reset);
+    reset.textContent = "Play Again?"
+    reset.addEventListener("click", () => {
+        document.location.reload();
+    })
 }
 
 function playRound(playerSelection, computerSelection) {
@@ -44,15 +55,17 @@ function playRound(playerSelection, computerSelection) {
         pScore.textContent = `${playerScore}`;
         if (isGameOver()) {
             title.textContent = `You Won!!!`
+            resetGame();
         }
     } else if ((playerSelection === "rock" && computerSelection === "paper") || 
         (playerSelection === "paper" && computerSelection === "scissors") || 
         (playerSelection === "scissors" && computerSelection === "rock")) {
-        title.textContent = `You Lost! ${computerSelection} beats ${playerSelection}`
+        title.textContent = `You Lost! ${computerSelection} beats ${playerSelection}`;
         giveScore("computer");
         cScore.textContent = `${computerScore}`;
         if (isGameOver()) {
             title.textContent = `You Lost!`;
+            resetGame();
         }
     }
 }
